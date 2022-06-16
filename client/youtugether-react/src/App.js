41cba9789005fb62d24e67999d0dd7636ededee0
Ben Costas <62489019+bencostas@users.noreply.github.com>
2022-06-16 16:04:28 -0400

    changed styling of buttons, almost done the landing page

--- client/youtugether-react/src/App.js
+++ client/youtugether-react/src/App.js
@@ -15,28 +15,36 @@ function App() {
       socket.emit("join_room", room);
       setShowChat(true);
     }
+    else {
+      alert("Username and Room ID must be filled to enter a room!")
+    }
   };
 
   return (
     <div className="App">
       {!showChat ? (
-        <div className="joinChatContainer">
-          <h3>Join A Chat</h3>
-          <input
+        <div>
+          <div className="flex flex-col gap-y-6 mb-6">
+            <input
+            class="self-center w-32 placeholder:text-black placeholder:text-center py-3 px-2 rounded-full bg-gray-200 enabled:text-center"
             type="text"
-            placeholder="John..."
+            placeholder="Username"
             onChange={(event) => {
               setUsername(event.target.value);
-            }}
-          />
-          <input
-            type="text"
-            placeholder="Room ID..."
-            onChange={(event) => {
-              setRoom(event.target.value);
-            }}
-          />
-          <button onClick={joinRoom}>Join A Room</button>
+            }}></input>
+            <button class="self-center w-36 bg-red-500 text-white py-3 px-2 rounded-full">Create Room</button>
+          </div>
+          <div className="flex flex-row gap-x-4">
+            <input
+              class="self-center w-32 placeholder:text-black placeholder:text-center py-3 px-2 rounded-full bg-gray-200 enabled:text-center"
+              type="text"
+              placeholder="Room ID"
+              onChange={(event) => {
+                setRoom(event.target.value);
+              }}
+            />
+            <button class="self-center w-36 bg-black text-white py-3 px-2 rounded-full" onClick={joinRoom}>Join Room</button>
+          </div>
         </div>
       ) : (
         <Room socket={socket} username={socket.id} room={room} />
